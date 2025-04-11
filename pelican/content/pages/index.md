cd4ff890a3d3983275e1d121e15e41d34c548a42
Tung Nguyen <tongueroo@gmail.com>
2025-04-11 19:38:04 +0000

    remove pelican

--- pelican/content/pages/index.md
+++
@@ -1,5 +0,0 @@
-Title: Welcome
-Date: 2024-01-01
-Category: General
-
-Welcome to the Pelican example site. This is a minimal static site built with Pelican.
--- pelican/pelicanconf.py
+++
@@ -1,9 +0,0 @@
-AUTHOR = 'Example Author'
-SITENAME = 'Pelican Example'
-SITEURL = ''
-
-PATH = 'content'
-OUTPUT_PATH = 'output'
-
-TIMEZONE = 'UTC'
-DEFAULT_LANG = 'en'
